5965d4f54506e782887f2848dfbb2f9227e3de61
mateussantosaquino-cyber <mateus.santos.aquino@escola.pr.gov.br>
2026-09-09 17:32:47 -0300

    atualizei o index

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao = document.querySelector("#botaoMensagem");
+const mensagem =document. querySeclector("#mensagemInterativa")
+botao. addEventListener("click", mostrarMensagem);
+function mostrarMensagem() {
+    mensagem.textContent=
+    "Vocẽ acabou de criar uma interação com JavaScript"
+}
