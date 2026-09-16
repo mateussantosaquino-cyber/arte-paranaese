523adc192dd52981f5d14d98059e2f173204297f
mateussantosaquino-cyber <mateus.santos.aquino@escola.pr.gov.br>
2026-09-16 17:18:59 -0300

    Ajustes no código

--- script.js
+++
@@ -1,7 +0,0 @@
-const botao = document.querySelector("#botaoMensagem");
-const mensagem =document. querySeclector("#mensagemInterativa")
-botao. addEventListener("click", mostrarMensagem);
-function mostrarMensagem() {
-    mensagem.textContent=
-    "Vocẽ acabou de criar uma interação com JavaScript"
-}
