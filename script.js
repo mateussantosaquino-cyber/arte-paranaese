3dddeeca8af7c7523fef52099295e00afa7b2de5
mateussantosaquino-cyber <mateus.santos.aquino@escola.pr.gov.br>
2026-09-16 17:38:51 -0300

    ajustei o código

---
+++ script.js
@@ -0,0 +1,15 @@
+const botoesCurtir = documement.querySeclectoeAll(".curtir");
+botoesCutir.forEach (function(botaoCurtir){
+    let curtiur = false;
+    botaoCurtir.adddEventListener("click", curtir);
+function curtir(){
+    const contador =botaoCutir,querySelector("span");
+    if(curtiur === false){
+        contador.textContent++;
+        curtiur = true;}
+        else{
+            contador.textContent--;
+            curtiu =false;
+        }
+}
+}):
